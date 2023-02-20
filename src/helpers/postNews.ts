8c268c46c3f83b081ef655e5cdc3af0904c289fb
Arianna <arianna.cella@gmail.com>
2023-02-20 14:53:40 +0100

    FINE

--- src/helpers/postNews.ts
+++ src/helpers/postNews.ts
@@ -1,7 +1,6 @@
 import { News } from "../types/typeApp";
 
 const postNews = async (news: News) => {
-    console.log("siamo nel post use stampa:"+ news.name+" "+news.surname+" "+news.email);
     const fetchApi = await fetch('/generateNews',{
         method: 'POST',
         body: JSON.stringify(news),
